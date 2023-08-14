09065b17aefe9fa5f7700594a372e7a8cda87d34
danny-817 <d_halsall@hotmail.com>
2023-08-14 15:55:33 +0100

    moved code for task 3 into api.controller

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require("express");
 const app = express();
+const getApiList = require("./controllers/api.controller");
 const getTopicsController = require("./controllers/topics.controller");
 const fs = require("fs/promises");
 
 app.get("/api/topics", getTopicsController);
 
-app.get("/api", (request, response) => {
-  fs.readFile("endpoints.json", "utf-8").then((apiJson) => {
-    response.send(apiJson);
-  });
-});
+app.get("/api", getApiList);
 
 app.use((err, request, response, next) => {
   response.status(500).send({ msg: "Internal Server Error" });
